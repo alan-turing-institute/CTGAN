3a6e8e6427fa4a33a63d41839003c6eeed92709d
Callum Mole <cmole@turing.ac.uk>
2022-09-12 15:19:37 +0100

    remove logging

--- ctgan/synthesizer.py
+++ ctgan/synthesizer.py
@@ -9,13 +9,6 @@ from ctgan.models import Discriminator, Generator
 from ctgan.sampler import Sampler
 from ctgan.transformer import DataTransformer
 
-import logging
-from logging.config import fileConfig
-dirname = path.dirname(__file__)
-logconfig = path.join(dirname, './logging_config.ini')
-fileConfig(logconfig)
-logger = logging.getLogger(__name__)
-
 
 class CTGANSynthesizer(object):
     """Conditional Table GAN Synthesizer.
@@ -233,9 +226,6 @@ class CTGANSynthesizer(object):
                 optimizerG.zero_grad()
                 loss_g.backward()
                 optimizerG.step()
-
-            logger.debug("Epoch %d, Loss G: %.4f, Loss D: %.4f" %
-                  (i + 1, loss_g.detach().cpu(), loss_d.detach().cpu()))
 
     def sample(self, n):
         """Sample data similar to the training data.
